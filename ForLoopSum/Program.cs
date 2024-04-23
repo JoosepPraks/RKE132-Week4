de85c7382fac36637c518a7a000270f8b558fdde
JoosepPraks <joosep.praks@tktk.ee>
2024-04-23 14:06:48 +0300

    add week4 code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,10 @@
+﻿int sum = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"i võrdub: {i}");
+    Console.WriteLine($"Praegune summa: {sum}");
+    sum = sum + i;
+}
+
+Console.WriteLine($"Lõpp summa: {sum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿// For loop
+
+int i = 0;
+
+for (i = 0; i < 5; i++) // i++ = i + 1
+{
+    Console.WriteLine("Meil pole haridust vaja!");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Head päeva");
